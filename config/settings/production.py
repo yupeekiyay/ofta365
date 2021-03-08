ca3bb9994f8397607754ed8e68e47d8e1dd35eaa
yupeekiyay <76213756+yupeekiyay@users.noreply.github.com>
2021-03-08 21:20:43 +0300

    storages

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,6 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-DJANGO_ALLOWED_HOSTS=['oftalmosonline.herokuapp.com']
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
 # DATABASES
 # ------------------------------------------------------------------------------
