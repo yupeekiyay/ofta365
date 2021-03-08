05dcd4318a31a231334fc71ac0872e862e9ea54e
yupeekiyay <76213756+yupeekiyay@users.noreply.github.com>
2021-03-08 18:43:10 +0300

    static files

--- config/settings/base.py
+++ config/settings/base.py
@@ -153,8 +153,7 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # MEDIA
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#media-root
