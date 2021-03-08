41e6a96944624525c3d6c8fffdd36415c5ebf095
yupeekiyay <76213756+yupeekiyay@users.noreply.github.com>
2021-03-08 18:58:12 +0300

    small errors

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-DJANGO_ALLOWED_HOSTS=['https://oftalmosonline.herokuapp.com']
+DJANGO_ALLOWED_HOSTS=['oftalmosonline.herokuapp.com']
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
 
 # DATABASES
