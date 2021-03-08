cd3f24381946d1e2c8c3200b3325730930c04d31
yupeekiyay <76213756+yupeekiyay@users.noreply.github.com>
2021-03-08 19:32:48 +0300

    settings magic

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-DJANGO_ALLOWED_HOSTS=['oftalmosonline.herokuapp.com']
+DJANGO_ALLOWED_HOSTS=['oftalmosonline.herokuapp.com', 'planedo.digital','www.planedo.digital']
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
 
 # DATABASES
