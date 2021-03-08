542ff76a754e7c22a59ee646ec400acb74c2d163
yupeekiyay <76213756+yupeekiyay@users.noreply.github.com>
2021-03-08 22:07:04 +0300

    added allowed-hosts variable to production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,6 +6,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+DJANGO_ALLOWED_HOSTS=['oftalmosonline.herokuapp.com']
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
 # DATABASES
 # ------------------------------------------------------------------------------
